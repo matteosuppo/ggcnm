7a7402b5f00d372d13dfe958f095f6890f23d5d6
Matteo Suppo <matteo.suppo@gmail.com>
2017-06-07 18:24:53 +0200

    Add some cards

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,79 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  common = [
+    {
+      title: 'Pinguino',
+      type: 'uccello carino',
+      rules: 'Batte sempre il Ghiro',
+      flavour: 'I pinguini vanno in giro, vanno in giro con il ghiro...',
+    },
+    {
+      title: 'Ghiro',
+      type: 'pallina carina',
+      rules: 'Batte sempre il Tonno',
+      flavour: '...ma il ghiro ha sempre sonno ...'
+    },
+    {
+      title: 'Riccio',
+      type: 'puntaspilli carino'
+    },
+    {
+      title: 'Miao',
+      type: 'assassino carino'
+    },
+    {
+      title: 'Polvere',
+      type: 'sporcizia carina'
+    },
+    {
+      title: 'Tonno',
+      type: 'cibo in scatola',
+      rules: 'Batte sempre il Pinguino',
+      flavour: '... e allora se ne vanno con il tonno'
+    },
+    {
+      title: 'Pizza',
+      type: 'cibo piatto'
+    },
+    {
+      title: 'Kebab',
+      type: 'cibo salutare'
+    },
+    {
+      title: 'Hummus',
+      type: 'cibo cremoso'
+    },
+    {
+      title: 'Gelato',
+      type: 'cibo dietetico'
+    },
+    {
+      title: 'Cavolo',
+      type: 'verdura misteriosa',
+      rules: '',
+      flavour: '... e questa cos\'è? La verdura de lo dimonio?'
+    },
+    {
+      title: 'Pachino',
+      type: 'verdura esotica'
+    },
+    {
+      title: 'Erre',
+      type: 'lettera immaginaria'
+    },
+    {
+      title: 'Ci',
+      type: 'lettera muta'
+    },
+    {
+      title: ''
+    },
+    {
+      title: ''
+    },
+    {
+      title: ''
+    },{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}
+  ];
 }
